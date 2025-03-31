476db4b00e79e272c75952b07b6dcd467b47b58c
saravanesh <saravaneshd@gmail.com>
2025-03-31 21:42:09 +0530

    updated

---
+++ Azure_Resource-RG/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~>4.10.0"
+    }
+  }
+required_version = ">= 1.5.0"
+}
+#provider "azurerm" {
+ ##   features {
+      
+  #  }
+#subsubscription_id = "a028cb2d-f614-40fe-bbe1-cbd3fde8da46"  
+#}
+
+provider "azurerm" {
+    features {}
+  subscription_id = "a028cb2d-f614-40fe-bbe1-cbd3fde8da46"
+  # other provider properties
+}
+
+# other resources
+
+# Create a resource group
+resource "azurerm_resource_group" "MIC-RG" {
+  name     = "TERRA-RG"
+  location = "WestUS"
+}
+
+
